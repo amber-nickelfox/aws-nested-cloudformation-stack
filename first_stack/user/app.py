571bc348c4483e91e1dcce61dd4bea063b3beb36
gautamamber <ambergautam1@gmail.com>
2019-12-04 01:29:30 +0530

    feat():created nested stack application

---
+++ first_stack/user/app.py
@@ -0,0 +1,13 @@
+import json
+
+# import requests
+
+
+def handler(event, context):
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world from user",
+        }),
+    }
